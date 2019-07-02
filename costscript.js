3a4929c292758e514ac63b1e626472b2df8da98d
craxel <52324896+craxel@users.noreply.github.com>
2019-07-02 14:51:08 +0200

    Update costscript.js

--- costscript.js
+++ costscript.js
@@ -7,7 +7,7 @@ jQuery(function() {
         newCosts;
       //the function changes the html value of the price, to show the correct price in the price div
       function recount() {
-        costs = newCosts.toFixed(2);
+        costs = newCosts;
         costsString = "" + costs;
         finalCosts = costsString.replace('.', ',');
         document.getElementById('pricePlaceholder').innerHTML=finalCosts;
